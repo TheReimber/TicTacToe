f1c8cc6012fed8b21b50f29c476f9783207a0e77
reimber <reimber@gmail.com>
2023-11-16 17:39:30 +0000

    Updated goalcheck function

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ class NC():
             self.pos.append(self.i)
         return
     def board(self):
+        print(self.pos)
         print(self.pos[0]," | ",self.pos[1]," | ",self.pos[2]," ")
         print("---+-----+----")
         print(self.pos[3]," | ",self.pos[4]," | ",self.pos[5]," ")
@@ -57,32 +58,32 @@ class NC():
         else:
             self.player(int(self.num))
     def goal_check(self):
-        if (self.pos[0] and self.pos[1] and self.pos[2] == 'X') or \
-                (self.pos[3] and self.pos[4] and self.pos[5] == 'X') or \
-                (self.pos[6] and self.pos[7] and self.pos[8] == 'X') or \
-                (self.pos[0] and self.pos[3] and self.pos[6] == 'X') or \
-                (self.pos[1] and self.pos[4] and self.pos[7] == 'X') or \
-                (self.pos[2] and self.pos[5] and self.pos[8] == 'X') or \
-                (self.pos[0] and self.pos[4] and self.pos[8] == 'X') or \
-                (self.pos[2] and self.pos[4] and self.pos[6] == 'X'):
+
+        if (self.pos[0] == 'X' and self.pos[1] == 'X' and self.pos[2] == 'X') or \
+                (self.pos[3] == 'X' and self.pos[4] == 'X' and self.pos[5] == 'X') or \
+                (self.pos[6] == 'X' and self.pos[7] == 'X' and self.pos[8] == 'X') or \
+                (self.pos[0] == 'X' and self.pos[3] == 'X' and self.pos[6] == 'X') or \
+                (self.pos[1] == 'X' and self.pos[4] == 'X' and self.pos[7] == 'X') or \
+                (self.pos[2] == 'X' and self.pos[5] == 'X' and self.pos[8] == 'X') or \
+                (self.pos[0] == 'X' and self.pos[4] == 'X' and self.pos[8] == 'X') or \
+                (self.pos[2] == 'X' and self.pos[4] == 'X' and self.pos[6] == 'X'):
             clear()
             TTT.board()
             print("Player Wins")
             TTT.again()
 
-        if (self.pos[0] and self.pos[1] and self.pos[2] == 'O') or \
-                (self.pos[3] and self.pos[4] and self.pos[5] == 'O') or \
-                (self.pos[6] and self.pos[7] and self.pos[8] == 'O') or \
-                (self.pos[0] and self.pos[3] and self.pos[6] == 'O') or \
-                (self.pos[1] and self.pos[4] and self.pos[7] == 'O') or \
-                (self.pos[2] and self.pos[5] and self.pos[8] == 'O') or \
-                (self.pos[0] and self.pos[4] and self.pos[8] == 'O') or \
-                (self.pos[2] and self.pos[4] and self.pos[6] == 'O'):
+        if (self.pos[0] == 'O' and self.pos[1] == 'O' and self.pos[2] == 'O') or \
+                (self.pos[3] == 'O' and self.pos[4] == 'O' and self.pos[5] == 'O') or \
+                (self.pos[6] == 'O' and self.pos[7] == 'O' and self.pos[8] == 'O') or \
+                (self.pos[0] == 'O' and self.pos[3] == 'O' and self.pos[6] == 'O') or \
+                (self.pos[1] == 'O' and self.pos[4] == 'O' and self.pos[7] == 'O') or \
+                (self.pos[2] == 'O' and self.pos[5] == 'O' and self.pos[8] == 'O') or \
+                (self.pos[0] == 'O' and self.pos[4] == 'O' and self.pos[8] == 'O') or \
+                (self.pos[2] == 'O' and self.pos[4] == 'O' and self.pos[6] == 'O'):
             clear()
             TTT.board()
             TTT.again()
             print("Computer Wins")
-
 
     def again(self):
         self.playagain = input("Play again y/n ")
